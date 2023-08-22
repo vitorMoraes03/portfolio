f735f12587e490577fa05947cf111d1c30132e18
Vitor Moraes <vitoristvan@hotmail.com>
2023-08-22 18:29:35 -0300

    feat: header

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,8 +3,6 @@ import { useTranslations } from 'next-intl';
 import Header from '@/components/header';
 
 export default function Home() {
-  const t = useTranslations('Index');
-  // return <h1 className='text-2xl border'>{t('title')}</h1>;
   return (
     <div>
       <Header />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,33 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import Link from 'next/link';
+
 function Header() {
+  const t = useTranslations('Header');
+
   return (
-    <nav>
-      <div>
-        <h3>Vitor Moraes</h3>
+    <nav className="xs:items-center flex justify-between md:grid md:grid-cols-12">
+      <div className="col-span-5 hidden md:inline-block">
+        <h3>{t('logo')}</h3>
       </div>
-      <div>
-        <button>About Me</button>
-        <button>Projects</button>
-        <button>Contact</button>
+      <div className="xs:gap-6 xs:flex-row col-span-6 flex flex-col items-start gap-1 lg:gap-10">
+        <button>{t('index.about')}</button>
+        <button>{t('index.projects')}</button>
+        <button>{t('index.contact')}</button>
+        <button>{t('index.certificates')}</button>
+      </div>
+      <div className="col-span-1 h-fit justify-self-end">
+        <Link href={'/teste'}>
+          <Image
+            src={'/images/br.png'}
+            alt="brazil flag"
+            width={20}
+            height={20}
+            title="pt-BR"
+          />
+        </Link>
       </div>
     </nav>
   );
