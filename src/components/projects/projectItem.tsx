6ca2598c7fd789811c3c1a756f14de8202651b8b
Vitor Moraes <vitoristvan@hotmail.com>
2023-08-28 14:12:41 -0300

    feat: tests for projects

--- src/components/projects/projectItem.tsx
+++ src/components/projects/projectItem.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import ListItem from './listItem';
 import { useState } from 'react';
+import { url } from 'inspector';
 
 interface ProjectItemProps {
   keyItem: number;
@@ -17,13 +18,28 @@ function ProjectItem({
     number | null
   >(null);
 
+  // style={{
+  //   backgroundImage: `url('/images/vitoristvan_urban_environment_at_night_1.png')`,
+  // }}
+
   return (
-    <div className="grid md:grid-cols-2 py-10">
+    // <div className="grid py-10 md:grid-cols-2">
+    <div className="py-10">
       <div>
-        <h2 className="text-3xl">
+        <div
+          style={{
+            backgroundImage: `url('/images/vitoristvan_urban_environment_at_night_1.png')`,
+          }}
+          className="h-20 rounded-sm bg-cover bg-center opacity-90"
+        >
+          teste
+        </div>
+      </div>
+      <div>
+        {/* <h2 className="bg-opacity-60 bg-cover text-3xl">
           {t(`projects.${keyItem}.name`)}
-        </h2>
-        <div className="max-w-[80%] pt-6">
+        </h2> */}
+        {/* <div className="max-w-[80%] pt-6">
           <ul className="flex flex-wrap gap-[2px]">
             {keysSkills.map((keySkill) => (
               <li
@@ -36,7 +52,7 @@ function ProjectItem({
               </li>
             ))}
           </ul>
-        </div>
+        </div> */}
         <div className="pt-6">
           <ul>
             <ListItem
@@ -60,24 +76,29 @@ function ProjectItem({
           </ul>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-start gap-4">
+      {/* <div className="flex flex-col items-center justify-start gap-4">
         <Image
           src={t(`projects.${keyItem}.imageUrl`)}
           alt="website shoes"
           width={350}
           height={150}
-          className="object-fill h-60 w-60 cursor-pointer rounded-xl 
+          className="h-60 w-60 cursor-pointer rounded-xl object-fill 
           filter transition-all duration-500 ease-in-out hover:scale-110 hover:drop-shadow-2xl"
         />
-        {/* <div>
+      </div> */}
+    </div>
+  );
+}
+
+{
+  /* <div>
           <button className="border border-black px-3 py-1 text-xs font-semibold">
             {t(`link`)}
           </button>
-        </div> */}
-        {/* LINK URL {t(`projects.${key}.repoUrl`)} */}
-      </div>
-    </div>
-  );
+        </div> */
+}
+{
+  /* LINK URL {t(`projects.${key}.repoUrl`)} */
 }
 
 export default ProjectItem;
